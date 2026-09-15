d92af9513dca4ea5e085083cb94ba47ce7a62d3b
kautukade <55291817+kautukade@users.noreply.github.com>
2026-09-15 14:17:52 +0530

    Load premium mobile responsive stylesheet

--- src/main.tsx
+++ src/main.tsx
@@ -2,6 +2,7 @@ import React from "react";
 import ReactDOM from "react-dom/client";
 import "./index.css";
 import "./premium.css";
+import "./mobile-premium.css";
 import App from "./App.tsx";
 
 ReactDOM.createRoot(document.getElementById("root")!).render(
